0a04150dfeecbd12cf5f891ba871f41737ff5192
Suji Ahn <137012201+deltawing71911@users.noreply.github.com>
2024-04-26 17:24:16 +0900

    Title: 비교 연산, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240426/비교 연산/comparison-operator.py
@@ -0,0 +1,11 @@
+imp = input()
+arr = imp.split()
+a = int(arr[0])
+b = int(arr[1])
+
+print(int(a >= b))
+print(int(a > b))
+print(int(b >= a))
+print(int(b > a))
+print(int(a == b))
+print(int(a != b))
