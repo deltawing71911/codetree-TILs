c278cecc71ea421945fdfdf33394557ad819ac50
Suji Ahn <137012201+deltawing71911@users.noreply.github.com>
2024-04-26 11:11:07 +0900

    Title: 체질량지수, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240426/체질량지수/bmi.py
@@ -0,0 +1,11 @@
+inp = input()
+arr = inp.split()
+h = int(arr[0])
+w = int(arr[1])
+b = int((100 * 100 * w) / (h * h))
+
+print(b)
+if int(b) >= 25:
+    print("Obesity")
+else:
+    print("Normal")
