2dd81aeaf73e833e2289ce0a64659a40b50bf16b
Suji Ahn <137012201+deltawing71911@users.noreply.github.com>
2024-04-26 20:32:26 +0900

    Title: 점수 비교, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240426/점수 비교/score-comparison.py
@@ -0,0 +1,14 @@
+inp = input()
+arr = inp.split()
+a_math = int(arr[0])
+a_eng = int(arr[1])
+
+inp = input()
+arr = inp.split()
+b_math = int(arr[0])
+b_eng = int(arr[1])
+
+if a_math > b_math and a_eng > b_eng:
+    print(1)
+else:
+    print(0)
