1a721198a6619ba6e4cfcbb3d42f883da9846b02
Suji Ahn <137012201+deltawing71911@users.noreply.github.com>
2024-04-27 21:17:29 +0900

    Title: 최댓값 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240427/최댓값 구하기/maximum-value.py
@@ -0,0 +1,17 @@
+inp = input()
+arr = inp.split()
+a = arr([0])
+b = arr([1])
+c = arr([2])
+
+if a >= b:
+    if a >= c:
+        print(a)
+    else:
+        print(c)
+    
+else:
+    if b >= c:
+        print(b)
+    else:
+        print(c)
