72800928a386b7e517dc23dee13095f6263824c9
Suji Ahn <137012201+deltawing71911@users.noreply.github.com>
2024-04-27 19:05:06 +0900

    Title: 좀 더 어려운 수학 점수, Time: 177ms, Memory: 24MB, Status: Passed - CodeTree

--- 240427/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
+++ 240427/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -1,12 +1,12 @@
-inp = input()
-arr = inp.split()
-a_math = int(arr[0])
-a_eng = int(arr[1])
+inp1 = input()
+arr1 = inp1.split()
+a_math = int(arr1[0])
+a_eng = int(arr1[1])
 
 inp2 = input()
 arr2 = inp2.split()
-b_math = int(arr[0])
-b_eng = int(arr[1])
+b_math = int(arr2[0])
+b_eng = int(arr2[1])
 
 if a_math > b_math or (a_math == b_math and a_eng > b_eng):
     print("A")
